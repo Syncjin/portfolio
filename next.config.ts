501bdce41afa0a2112b9888cb8f8e248864eb5f5
이우진 <dldndldms@gmail.com>
2025-06-29 18:06:11 +0900

    feat: webpack 설정 수정 - 프로덕션 환경에서 console.log 제거를 위한 컴파일러 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -5,30 +5,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   distDir: "dist",
   output: "standalone",
-  webpack: (config, { dev, isServer }) => {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       "@": path.join(__dirname, "src"),
     };
-
-    // Production 빌드에서 console.log 제거
-    if (!dev && !isServer) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const minimizers = config.optimization.minimizer as any[];
-      if (minimizers && minimizers.length > 0) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const terserPlugin = minimizers.find((minimizer: any) => minimizer.constructor.name === "TerserPlugin");
-        if (terserPlugin && terserPlugin.options) {
-          terserPlugin.options.terserOptions = {
-            ...terserPlugin.options.terserOptions,
-            compress: {
-              ...terserPlugin.options.terserOptions?.compress,
-              drop_console: true,
-            },
-          };
-        }
-      }
-    }
 
     return config;
   },
